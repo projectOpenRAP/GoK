d6e8de087a4de1f40eacdfe90aa33b11719d14f5
rak16 <ravishahmad16@gmail.com>
2018-07-06 00:14:26 +0530

    Add telemetry authentication

--- gok/config/config.js
+++ gok/config/config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    FS_ROOT : '/home/admin/GoK'
+    FS_ROOT : '/home/admin/GoK',
+    username : 'brandonStark',
+    password : 'theBuilder',
+    serverIP : '35.240.131.174',
+    telemetryAPI : {
+        init : `http://35.240.131.174:8888/api/auth/v1/init`, // Authenticates user and returns a token
+        hello : `http://35.240.131.174:8000/api/auth/v1/hello`, // Checks if the user is authenticated
+        upload : `http://35.240.131.174:8000/api/auth/v1/telemetry/couchbase` // Load telemetry
+    },
+    keyFile : '/home/admin/key'
 }
--- gok/gok.telemetry.js
+++ gok/gok.telemetry.js
@@ -11,22 +11,102 @@ const {
 	zipContents
 } = require('../../../telemetrysdk');
 
+const config = require('./config/config.js');
+
 const plugin = 'gok';
+
+let storeKey = key => {
+    return fs.writeFileSync(config.keyFile, key);
+}
+
+let getKey = () => {
+    fs.openSync(config.keyFile, 'a+');
+    return fs.readFileSync(config.keyFile, 'utf-8');
+}
+
+let isAuthenticated = () => {
+    let defer = q.defer();
+
+    const url = config.telemetryAPI.hello;
+    const apikey = getKey();
+
+	if(apikey.length === 0) {
+        defer.resolve({
+            authenticated : false,
+            key : undefined
+        });
+    } else {
+		const options = {
+	        url,
+	        headers : { apikey }
+	    };
+
+	    request.get(options, (err, httpRes, body) => {
+	        if(err) { // Control would be sent to the catch block in this case
+	            console.log('Some error occured during authentication.');
+	            defer.reject(err);
+	        } else if(httpRes.statusCode === 403) {
+	            console.log('Forbidden.');
+	            defer.resolve({
+	                authenticated : false,
+	                key : apikey
+	            });
+	        } else {
+	            console.log('Allowed.');
+	            defer.resolve({
+	                authenticated : true,
+	                key : apikey
+	            });
+	        }
+	    });
+	}
+
+    return defer.promise;
+}
+
+let authenticateUser = () => {
+	let defer = q.defer();
+
+	const url = config.telemetryAPI.init;
+	const form = {
+		username : config.username,
+		password : config.password
+	};
+
+	request.post({ url, form }, (err, httpRes, body) => {
+		if(err) {
+            console.log('Error occured while authenticating user', config.username);
+			defer.reject(err);
+		} else {
+			console.log('Authenticating user...');
+			defer.resolve(JSON.parse(body));
+		}
+	});
+
+	return defer.promise;
+}
 
 let sendTelemetry = ({ fullPath, fullName, buffer }) => {
 	let defer = q.defer();
 
-	const url = 'http://35.187.232.200:8888/api/auth/v1/telemetry/couchbase';
+	const url = config.telemetryAPI.upload;
+	const apikey = getKey();
 	const formData = {
 		file : fs.createReadStream(fullPath)
 	};
 
-	request.post({ url, formData }, (err, httpRes, body) => {
+	const options = {
+		url,
+		formData,
+		headers : { apikey }
+	};
+
+	request.post(options, (err, httpRes, body) => {
 		if(err) {
 			defer.reject(err);
 		} else {
-			console.log('Telemetry sent. Response:', body);
-			defer.resolve({ success : true });
+			console.log('Sending telemetry...');
+			defer.resolve({ success : true, body });
 		}
 	});
 
@@ -38,13 +118,44 @@ let initiateTelemetrySync = () => {
 
     cron.schedule('*/30 * * * * *', () => {
         isInternetActive()
-            .then(() => getTelemetryData(plugin, 1))
+			.then(isAuthenticated)
+			.then(res => {
+		        if(!res.authenticated) {
+		            return authenticateUser();
+		        } else {
+		            return {
+		                success : true,
+		                keyCached : true,
+		                key : res.key
+		            };
+		        }
+		    })
+		    .then(res => {
+		        if(res.success) {
+					if(!res.keyCached) {
+						console.log('User authenticated.');
+						storeKey(res.key);
+					}
+					// console.log('Response:', res);
+
+					return getTelemetryData(plugin, 1)
+		        } else {
+		            throw 'Couldn\'t authenticate user.';
+		        }
+		    })
             .then(res => {
 				if(res.success) {
 					const data = zipContents(plugin, res.data);
 					return sendTelemetry(data)
 				} else {
 					throw res.msg;
+				}
+			})
+			.then(res => {
+				if(res.success) {
+					console.log('Telemetry synced. Server response:', res.body);
+				} else {
+					throw 'Error occurred while syncing telemetry. Server response: ' + res.body;
 				}
 			})
             .catch(error => {
